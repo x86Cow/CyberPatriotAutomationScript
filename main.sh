29dbfaafcc6a3c1ef215c0d5edd5218be695dbf7
x86Cow <DrCow@protonmail.com>
2022-10-28 13:40:37 +0000

    fixed missing quote error

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ echo "System Updated!"
 # remove unwanted files
 find / -name *.jpg 2> /dev/null 1> fileList.txt
 find / -name *.mp3 2> /dev/null 1> fileList.txt
-echo "fileList.txt created!
+echo "fileList.txt created!"
 
 # edit FileList to remove files from list that are wanted
 vi fileList.txt
