afebb9d3da22fe996f73324cf2e6c6026ed9465b
x86Cow <DrCow@protonmail.com>
2022-10-28 13:38:23 +0000

    added ability to edit fileList

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
 #!bin/sh
 
 # UPDATES
-sudo apt update 1> /dev/null 
-sudo apt upgrade 1> /dev/null
+sudo apt-get update 1> /dev/null 
+sudo apt-get upgrade 1> /dev/null
 echo "System Updated!"
 
 # remove unwanted files
 find / -name *.jpg 2> /dev/null 1> fileList.txt
 find / -name *.mp3 2> /dev/null 1> fileList.txt
-echo "fileList.txt created!"
+echo "fileList.txt created!
 
+# edit FileList to remove files from list that are wanted
+vi fileList.txt
